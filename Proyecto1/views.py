b12a5be7a9a44b9ca5f166322f6246aff570f77e
Davil-rafesk <rafesk2404@gmail.com>
2022-02-23 19:50:10 -0400

    Transformé una vista, quité todo el código html a una plantilla en un archivo distinto, de esta manera se puede llamar de forma independiente.

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,20 +1,20 @@
 from datetime import datetime
+from django.template import Template, Context
 from django.http import HttpResponse
 
 
 def saludo(request):    # A cada función que creamos en el archivo Views se le denomina vista. 
                         # El objetivo es devolver una respuesta donde aparezca el texto.
 
-    domumento = """
-    <html>
-    <body>
-    <h1>
-    Hola gente, esta es la primera página creada con Django.
-    </h1>
-    </body>
-    </html>
-    """
+    doc_externo = open("E:/respaldoC/Programación/Django/ProyectosDjango/Proyecto1/Proyecto1/Plantillas/mi_plantilla.html")
 
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx = Context()
+
+    domumento = plt.render(ctx)
 
     return HttpResponse(domumento)
 
