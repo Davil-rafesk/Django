87417aae185810a8d916d5940ab73fb23f7e0898
Davil-rafesk <rafesk2404@gmail.com>
2022-02-22 23:59:57 -0400

    Mi primera pagina con Django.

---
+++ Proyecto1/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def saludo(request):    # A cada función que creamos en el archivo Views se le denomina vista. 
+                        # El objetivo es devolver una respuesta donde aparezca el texto.
+
+    return HttpResponse("Hola gente, esta es la primera pagian creada con Django.")
+
+def despedida(request):
+
+    return HttpResponse("Esta vista es la función de despedida.")
